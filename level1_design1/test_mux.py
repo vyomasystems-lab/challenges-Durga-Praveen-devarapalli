a1e0aa62fdfa7367b5ccc23af318db026d7b5363
Durga-Praveen-devarapalli <107881731+Durga-Praveen-devarapalli@users.noreply.github.com>
2022-07-29 21:44:41 +0530

    Update test_mux.py

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -2,9 +2,11 @@
 
 import cocotb
 from cocotb.triggers import Timer
-
+import random
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
-
+    dut.sel.value=0
+    dut.inp1.value=2
+    print(dut.out.value)
     cocotb.log.info('##### CTB: Develop your test here ########')
